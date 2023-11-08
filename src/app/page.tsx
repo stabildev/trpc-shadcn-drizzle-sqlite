2570e3110f40f84b04f04cb28061d00d0ddf7186
stabildev <kleehenrik@icloud.com>
2023-11-09 00:32:42 +0100

    fix folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { serverClient } from '@/app/_trpc/serverClient'
 
-import { TodoList } from '@/app/components/TodoList'
+import { TodoList } from '@/components/TodoList'
 
 export default async function Home() {
   const todos = await serverClient.getTodos()
